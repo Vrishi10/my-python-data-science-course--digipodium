b7eab970713db9e9026bdba7b8a4c9238f3366ad
Vrishi10 <115559460+Vrishi10@users.noreply.github.com>
2022-11-24 18:38:16 +0530

    games_ex2.py

--- functions/games_ex1.py
+++ functions/games_ex1.py
@@ -1,12 +1,22 @@
 import pgzrun
-HEIGHT = 300
+HEIGHT = 600
 WIDTH = 800
 p = Actor('ironman',(100,100))
 c = Actor('cookie_img')
 
 def draw():
-    
+    screen.fill('white')
     p.draw()
     c.draw()
+    print('drawing')
 
+def update():
+    # print('updating')
+    p.x += 3
+    p.angle = -10
+    if p.x > WIDTH:
+        p.x = 0
+    # print(f'p.x = {p.x}')
+    print(p.x , p.y)
+    
 pgzrun.go()
