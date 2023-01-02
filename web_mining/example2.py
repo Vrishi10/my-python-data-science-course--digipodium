3cd29f05a8777af8646092aa47c6c9d57f689b72
Vrishi10 <115559460+Vrishi10@users.noreply.github.com>
2023-01-02 18:34:12 +0530

    web_mining

---
+++ web_mining/example2.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'http://www.google.com/search?q=css'
+response = requests.get(url)
+if not response.status_code == 200:
+    print('An error has occured.')
+    exit()
+else:
+    print('Success!')
+    soup = BeautifulSoup(response.text, 'html5lib')
+    print(soup.prettify())
+
+
+    # get all heading h3
+    headings_3 = soup.find_all('h3')
+    print("Headings 3:")
+    for i in headings_3:
+        print(i.text)
+
+ # get all links
+links = soup.find_all('a')
+print("Links:")
+for i in links:
+    print(f'{i.text}  {i.get("href")}')
+
+     
